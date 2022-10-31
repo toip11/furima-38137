980e02974159c5fad686b884faf6599fb3ae01a6
toip11 <109015887+toip11@users.noreply.github.com>
2022-10-31 11:57:02 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_reading, presence: true
-  validates :first_name_reading, presence: true
+  validates :password, presence: true, format: { with:  /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_reading, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :first_name_reading, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
   validates :birthday, presence: true
 end
