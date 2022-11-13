6906f861051664a748d99beab3ce3444f53a662d
toip11 <109015887+toip11@users.noreply.github.com>
2022-11-13 18:01:27 +0900

    JavaScript修正

--- app/javascript/packs/item_price.js
+++ app/javascript/packs/item_price.js
@@ -6,10 +6,10 @@ window.addEventListener('load', () => {
     const SalesProfit = document.getElementById("profit");
     const TAX = 10
     function total(price, tax) {
-      return Math.round(price * (tax / 100));
+      return Math.floor(price * (tax / 100));
     }
     function difference(price, tax) {
-      return Math.round(price - (price * (tax / 100)));
+      return Math.floor(price - (price * (tax / 100)));
     }
     addTaxDom.innerHTML = (total(inputValue, TAX))
     SalesProfit.innerHTML = (difference(inputValue, TAX))
