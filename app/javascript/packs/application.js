9ec1fc8c266e5ea8dd49efa2c4ff600de0e6b267
toip11 <109015887+toip11@users.noreply.github.com>
2022-11-20 16:47:37 +0900

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,8 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("/Users/toip/project/furima-38137/app/javascript/packs/item_price.js");
+require("./item_price")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
