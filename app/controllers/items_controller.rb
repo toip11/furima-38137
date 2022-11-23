a50e25352604709bf70a8e36e0b730087d1c942f
toip11 <109015887+toip11@users.noreply.github.com>
2022-11-23 17:30:28 +0900

    コントローラー見直し

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,22 +36,24 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy(move_to_index)
+    if item.user_id == current_user.id
+      item.destroy
+    end
     redirect_to root_path
   end
 
   private
-  def item_params
-    params.require(:item).permit(:name, :explanation, :category_id, :situation_id, :delivery_charge_id, :prefecture_id, :days_required_id, :price, :image).merge(user_id: current_user.id)
-  end
+    def item_params
+      params.require(:item).permit(:name, :explanation, :category_id, :situation_id, :delivery_charge_id, :prefecture_id, :days_required_id, :price, :image).merge(user_id: current_user.id)
+    end
 
-  def move_to_index
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to action: :index
+    def move_to_index
+      unless user_signed_in? && current_user.id == @item.user_id
+        redirect_to action: :index
+      end
+    end
+
+    def item_find
+      @item = Item.find(params[:id])
     end
   end
-
-  def item_find
-    @item = Item.find(params[:id])
-  end
-end
