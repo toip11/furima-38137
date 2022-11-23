c35bb96eb25232f9c7502aa2ef91d003be548045
toip11 <109015887+toip11@users.noreply.github.com>
2022-11-23 12:47:46 +0900

    商品詳細画面の情報表示実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,16 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+    @user = User.find(params[:id])
+    @delivery_charge = DeliveryCharge.find(@item.delivery_charge_id)
+    @category = Category.find(@item.category_id)
+    @days_required = DaysRequired.find(@item.days_required_id)
+    @prefecture = Prefecture.find(@item.prefecture_id)
+    @situation = Situation.find(@item.situation_id)
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :situation_id, :delivery_charge_id, :prefecture_id, :days_required_id, :price, :image).merge(user_id: current_user.id)
