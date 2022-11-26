f391c801bd8612cbe287343584c6773e24f8ad09
toip11 <109015887+toip11@users.noreply.github.com>
2022-11-26 18:11:34 +0900

    Formオブジェクトパターン作成

---
+++ app/models/purchase_record_shipping_to.rb
@@ -0,0 +1,17 @@
+class PurchaseRecordShippingTo
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id
+
+  with_option presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :municipality
+    validates :address
+    validates :phone_number, length: { only_integer: true, minimum: 10, maximum: 11 }, format: { with: /\A[0-9]+\z/ }
+  end
+  validates :prefecture_id, numericality: { other_than: 1 }
+
+  def save
+    purchase = PurchaseRecord.create(user_id: user_id, item_id: item_id)
+    ShippingTo.create(postal_code: postal_code, :prefecture; prefecture, municipality: municipality, sddress: address, building_name: building_name, phone_number: phone_number, purchase_record_id: purchase.id)
+  end
+end
--- app/models/shipping_to.rb
+++ app/models/shipping_to.rb
@@ -1,5 +1,6 @@
 class PurchaseRecord < ApplicationRecord
   belongs_to :item
   belongs_to :user
+  belongs_to :prefecture
   has_one :shipping_to
 end
